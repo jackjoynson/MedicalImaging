77a82310e15b26192e4b691d7c1191b9d95f77fb
Jack Joynson <JHJ560@student.bham.ac.uk>
2018-01-23 17:53:56 +0000

    Minor changes to main

--- ImageReconstruction/Master/main.cpp
+++ ImageReconstruction/Master/main.cpp
@@ -1,10 +1,16 @@
 #include "GetUserInputs.h"
 
 
+#include <string>
+
 int main()
 {
 	GetUserInputs GUI;
+	
 
+	std::string close;
+	cout << "Program complete. Enter something to close.." << endl;
+	cin >> close;
 
 	return 0;
 }
