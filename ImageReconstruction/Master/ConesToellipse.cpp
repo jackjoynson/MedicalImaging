ee6a23c0bc4eeb513c3b9ccc846b217a91c94676
ziyue wang <wangziyue@hotmail.co.uk>
2018-02-13 22:21:27 +0000

    commited wrong file

--- ImageReconstruction/Master/ConesToellipse.cpp
+++ ImageReconstruction/Master/ConesToellipse.cpp
@@ -10,77 +10,128 @@
 #include <cmath>
 #include <iostream>
 
+#define PI 3.14159265359
+
 ConesToEllipse::ConesToEllipse()
 {
 
 }
 
+/////
+//ConesToEllipse::ConesToEllipse(Vector3D plane, vector<Cone> cones):
+//    _cones(cones),
+//    _plane(plane)
+//{
+//    //Normal of the plane
+//    Vector3D planeNormal(0,0,1);
+
+
+//    for(int i = 0; i < _cones.size(); i++){
+
+//        //Convert cone into a vector
+//        Cone thisCone = _cones[i];
+//        double coneAxisX = thisCone.GetStartX() - thisCone.GetXImagePlane();
+//        double coneAxisY = thisCone.GetStartY() - thisCone.GetYImagePlane();
+//        Vector3D coneAxis(coneAxisX,coneAxisY,plane.getZ());            //Vector of the cone axis
+
+//        //Vector of where the cone intersects the plane
+//        Vector3D planePoint(0,0,plane.getZ());
+//        Vector3D coneVertex(thisCone.GetStartX(),thisCone.GetYImagePlane(),0);
+
+
+//        //Angles for calculating
+//        double cosTheta = planeNormal.Dot(coneAxis)/(norm(planeNormal)*norm(coneAxis));
+//        double cosThetaSq = cosTheta*cosTheta;
+//        double sinAlpha = sin(thisCone.GetHalfAngle());
+//        double sinAlphaSq = sinAlpha*sinAlpha;
+//        double cosAlpha = sqrt(1.0 - sinAlphaSq);
+
+//        double t = planeNormal.Dot(planePoint - coneVertex);
+//        double b = cosThetaSq - sinAlphaSq;
+//        double h = t/b;
+
+//        double diffX = coneAxis.getX()-cosTheta*planeNormal.getX();
+//        double diffY = coneAxis.getY()-cosTheta*planeNormal.getY();
+//        double diffZ = coneAxis.getZ()-cosTheta*planeNormal.getZ();
+//        Vector3D diff(diffX,diffY,diffZ);
+
+//        Vector3D ellipseU = diff/norm(diff);
+
+//        Vector3D center(coneVertex.getX() + h*cosTheta*coneAxis.getX() - h*sinAlphaSq*planeNormal.getX(),
+//                        coneVertex.getY() + h*cosTheta*coneAxis.getY() - h*sinAlphaSq*planeNormal.getY(),
+//                        coneVertex.getZ() + h*cosTheta*coneAxis.getZ() - h*sinAlphaSq*planeNormal.getZ()
+//                        );
+
+
+
+//        Vector3D u(coneAxis.getX()-cosTheta*planeNormal.getX(),
+//                    coneAxis.getY()-cosTheta*planeNormal.getY(),
+//                    coneAxis.getZ()-cosTheta*planeNormal.getZ());
+
+
+//        Vector3D majorAxis = normalize(u);
+//        Vector3D minorAxis = cross(planeNormal,ellipseU);
+//        double majorRadius = fabs(h) * sinAlpha*cosAlpha;
+//        double minorRadius = t*sinAlpha/sqrt(fabs(b));
+
+//        if(cosTheta > sinAlpha || cosTheta == 0){
+//            //Ellipse
+//            Ellipse thisEllipse(center,majorAxis,minorAxis,majorRadius,minorRadius);
+//            _ellipses.push_back(thisEllipse);
+//        }
+//        else
+//		{
+//            cout << "Ellipse " << i << " is invalid" << endl;
+//        }
+
+//    }
+
+//}
+
 ConesToEllipse::ConesToEllipse(Vector3D plane, vector<Cone> cones):
     _cones(cones),
     _plane(plane)
 {
-    //Normal of the plane
+
     Vector3D planeNormal(0,0,1);
 
 
     for(int i = 0; i < _cones.size(); i++){
-
-        //Convert cone into a vector
         Cone thisCone = _cones[i];
-        double coneAxisX = thisCone.GetStartX() - thisCone.GetXImagePlane();
-        double coneAxisY = thisCone.GetStartY() - thisCone.GetYImagePlane();
-        Vector3D coneAxis(coneAxisX,coneAxisY,plane.getZ());            //Vector of the cone axis
+        double theta = thisCone.GetHalfAngle();
 
-        //Vector of where the cone intersects the plane
-        Vector3D planePoint(0,0,plane.getZ());
-        Vector3D coneVertex(thisCone.GetStartX(),thisCone.GetYImagePlane(),0);
+        if(theta > PI/2){
+        }
 
+        else      //Change this later
+        {
+            double a = planeNormal.getZ();
+            double phi2 = atan(thisCone.GetXImagePlane()/a);
+            double phi1 = theta - phi2;
+            double u = a*tan(phi1);
+            double v = a*tan(phi2);
+            double rmin = u+v;
+            double rmax;
 
-        //Angles for calculating
-        double cosTheta = planeNormal.Dot(coneAxis)/(norm(planeNormal)*norm(coneAxis));
-        double cosThetaSq = cosTheta*cosTheta;
-        double sinAlpha = sin(thisCone.GetHalfAngle());
-        double sinAlphaSq = sinAlpha*sinAlpha;
-        double cosAlpha = sqrt(1.0 - sinAlphaSq);
+            if(phi2+theta > PI/2 || phi2+theta == PI/2){
+                //rmax = 0;           //Rmax doesn't exist, hyerbola.
+                cout << "Ellipse " << i << " is invalid" << endl;
+            }
+            else{
+                double vPlusT = a*tan(phi2+theta);
+                rmax = vPlusT - v;
 
-        double t = planeNormal.Dot(planePoint - coneVertex);
-        double b = cosThetaSq - sinAlphaSq;
-        double h = t/b;
-
-        double diffX = coneAxis.getX()-cosTheta*planeNormal.getX();
-        double diffY = coneAxis.getY()-cosTheta*planeNormal.getY();
-        double diffZ = coneAxis.getZ()-cosTheta*planeNormal.getZ();
-        Vector3D diff(diffX,diffY,diffZ);
-
-        Vector3D ellipseU = diff/norm(diff);
-
-        Vector3D center(coneVertex.getX() + h*cosTheta*coneAxis.getX() - h*sinAlphaSq*planeNormal.getX(),
-                        coneVertex.getY() + h*cosTheta*coneAxis.getY() - h*sinAlphaSq*planeNormal.getY(),
-                        coneVertex.getZ() + h*cosTheta*coneAxis.getZ() - h*sinAlphaSq*planeNormal.getZ()
-                        );
-
-
-
-        Vector3D u(coneAxis.getX()-cosTheta*planeNormal.getX(),
-                    coneAxis.getY()-cosTheta*planeNormal.getY(),
-                    coneAxis.getZ()-cosTheta*planeNormal.getZ());
-
-
-        Vector3D majorAxis = normalize(u);
-        Vector3D minorAxis = cross(planeNormal,ellipseU);
-        double majorRadius = fabs(h) * sinAlpha*cosAlpha;
-        double minorRadius = t*sinAlpha/sqrt(fabs(b));
-
-        if(cosTheta > sinAlpha || cosTheta == 0){
-            //Ellipse
-            Ellipse thisEllipse(center,majorAxis,minorAxis,majorRadius,minorRadius);
+            double majorRadius = (rmax+rmin)/2.0;           //These are the SEMI major and MINOR axis radiuses;
+            double minorRadius = sqrt(rmax*rmin);
+            Vector3D center(majorRadius,0,a);
+            Ellipse thisEllipse(center,Vector3D(1,0,0),Vector3D(0,1,0),majorRadius,minorRadius);
             _ellipses.push_back(thisEllipse);
         }
-        else
-		{
-            cout << "Ellipse " << i << " is invalid" << endl;
-        }
+
+
 
     }
 
+
+}
 }
